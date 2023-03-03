152815fdb5edb5f727988f9e751ada78beffe1fc
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-03-03 15:46:06 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioEntrevista3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExercicioEntrevista3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a string a ser invertida: ");
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+            int indexInicio = 0;
+            int indexFim = charArray.Length - 1;
+            while (indexInicio < indexFim)
+            {
+                char sup = charArray[indexInicio];
+                charArray[indexInicio] = charArray[indexFim];
+                charArray[indexFim] = sup;
+
+                indexInicio++;
+                indexFim--;
+            }
+            string stringInvertida = new string(charArray);
+            Console.WriteLine(stringInvertida);
+        }
+    }
+}
